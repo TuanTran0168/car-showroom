dc2b9d8ef64098cba700b0950eea8903e8911951
TuanTran0168 <tamthy12345@gmail.com>
2025-04-03 19:16:16 +0700

    [ADD][setup/entity-database] add Entity for CarShowroom

---
+++ CarShowroom/src/main/java/com/tuantran/CarShowroom/entity/Variant.java
@@ -0,0 +1,39 @@
+package com.tuantran.CarShowroom.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "spring_variant_001")
+public class Variant extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne(targetEntity = Car.class)
+    @JoinColumn(name = "car_id", referencedColumnName = "id", nullable = false)
+    private Car car;
+
+    @JsonIgnore
+    @ManyToMany(targetEntity = Feature.class, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "spring_variant_feature_001",
+            joinColumns = @JoinColumn(name = "variant_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "feature_id", referencedColumnName = "id")
+    )
+    @ToString.Exclude
+    private Set<Feature> featureSet;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "variant", targetEntity = FeatureValue.class)
+    @ToString.Exclude
+    private Set<FeatureValue> featureValueSet;
+}
